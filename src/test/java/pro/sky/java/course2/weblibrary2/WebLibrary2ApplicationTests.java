bb199429a8091e69188c047bed90171aeb21fca9
dns <pochti_zvezda@mail.ru>
2021-12-03 00:42:17 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/weblibrary2/WebLibrary2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.weblibrary2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibrary2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
